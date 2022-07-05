342d1cc969612de6d5fccf980bd01a61351ec5f3
PiyushKanathe <62841323+PiyushKanathe@users.noreply.github.com>
2022-07-05 18:57:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import RenderCountComp from './RenderCountComp';
-import TableDisply from './TableDisplay';
+import DisplayComp from './DisplayListByAxios';
 
 function App() {
   return (
@@ -16,10 +15,44 @@ function App() {
         </a>
       </header> */}
       <div>
-        <TableDisply></TableDisply>
+        <DisplayComp></DisplayComp>
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+// export default function App() {
+//   const [seconds, setSeconds] = useState(0);
+//   const [status, setStatus] = useState(false);
+
+//   useEffect(() => {
+//     let interval = setInterval(() => {
+//       setSeconds((seconds) => seconds + 1);
+//     }, 1000);
+//     if (!status) {
+//       clearInterval(interval);    
+//     }
+//   }, [status]);
+
+//   const start = () => {
+//     setStatus(true);
+//   };
+//   const stop = () => {
+//     setStatus(false);
+//   };
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {seconds} seconds have elapsed since mounting.
+//       </header>
+//       <div>
+//         <button onClick={start}>Start</button>
+//         <button onClick={stop}>Stop</button>
+//       </div>
+//     </div>
+//   );
+// }
